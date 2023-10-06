30691fef15cd4f5518e759e9f50ca0a23e015002
YariMorcus <yarimorcus@zeelandnet.nl>
2023-10-06 19:29:27 +0200

    Add comments and save past advices

--- src/js/model.js
+++ src/js/model.js
@@ -7,8 +7,12 @@ const state = {
     advice: null,
   },
   advicesGenerated: 0, // Statistic purposes
+  pastAdvices: [],
 };
 
+/**
+ * Fetch advice based on given API_URL (configuration value)
+ */
 export const fetchAdvice = async function () {
   try {
     // 1) Fetch advice data
@@ -17,6 +21,9 @@ export const fetchAdvice = async function () {
     // 2) Save advice data
     state.currentAdvice.id = data.slip.id;
     state.currentAdvice.advice = data.slip.advice;
+
+    // 3) Store past advices
+    storePastAdvices(data);
 
     // 3) Update statistics
     updateStatistics();
@@ -27,6 +34,20 @@ export const fetchAdvice = async function () {
   }
 };
 
+/**
+ * Increase generated advices with 1
+ * @returns {undefined}
+ */
 const updateStatistics = function () {
   state.advicesGenerated++;
+};
+
+/**
+ * Remember current advice when new advice is being generated.
+ *
+ * This prevents losing past generated advices.
+ * @returns {undefined}
+ */
+const storePastAdvices = function (currentAdvice) {
+  state.pastAdvices.push(currentAdvice);
 };
