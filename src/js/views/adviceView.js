ed0ac558eaa70366e117626592a95ff89a5c9e67
YariMorcus <yarimorcus@zeelandnet.nl>
2023-10-06 20:26:12 +0200

    Add handler & generate markup

--- src/js/views/adviceView.js
+++ src/js/views/adviceView.js
@@ -1,8 +1,25 @@
-class adviceView {
-  #buttonEl = document.querySelector('.js-advice__dice-btn');
+import View from './View.js';
+class adviceView extends View {
+  _parentEl = document.querySelector('.advice');
+  advice = document.querySelector('.advice__quote');
 
   addHandlerGenerate(handler) {
-    this.#buttonEl.addEventListener('click', handler);
+    this._parentEl.addEventListener('click', e => {
+      const btn = e.target.closest('.js-advice__dice-btn');
+
+      if (!btn) return;
+
+      handler();
+    });
+  }
+
+  _generateMarkup() {
+    return `
+      <h1 class="advice__title">Advice #${this._data.advicesGenerated}</h1>
+      <q class="advice__quote">${this._data.currentAdvice.advice}</q>
+      <hr class="advice__divider">
+      <button class="advice__dice-btn js-advice__dice-btn" aria-label="Generate advice"></button>
+    `;
   }
 }
 
